bddc434490076a3e8f6c1928963a13e525c9f0cc
userr2232 <reynaldo.rz.26@gmail.com>
2019-08-22 18:56:09 -0500

    done

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
+    string line;
+    bool first = true;
+    while(getline(cin,line)) {
+        string output = "";
+        for(int i = 0; i < line.length(); ++i) {
+            string to_insert;
+            if(line[i] == '"') {
+                if(first) to_insert = "``";
+                else to_insert = "\'\'";
+                first = !first;
+            }
+            else to_insert = line[i];
+            output = output + to_insert;
+        }
+        cout << output << endl;
+    }
     return 0;
 }
